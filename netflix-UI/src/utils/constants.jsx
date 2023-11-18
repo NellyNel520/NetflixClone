bd095325f69862c7646bf0ad8ad7eec6703a3054
NellyNel520 <channelharris520@gmail.com>
2023-11-17 22:14:53 -0500

    netflix api deployed sucessful and update mongo db base url constant within netflix-UI

--- netflix-UI/src/utils/constants.jsx
+++ netflix-UI/src/utils/constants.jsx
@@ -1,3 +1,4 @@
 export const API_KEY = "1b3318f6cac22f830b1d690422391493";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-export const MONGO_DB_BASE_URL = "http://localhost:3001/api";
+// export const MONGO_DB_BASE_URL = "http://localhost:3001/api";
+export const MONGO_DB_BASE_URL = "https://netflix-api-5ihm.onrender.com/api";
